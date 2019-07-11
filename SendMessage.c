08f2f89a4c1fb688f6d4a163e63dee00e2966746
ste <git@grundell.co.uk>
2019-07-11 16:48:03 +0100

    Added the main ingredient

---
+++ SendMessage.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#pragma comment (lib, "User32.lib")
+#pragma comment (lib, "Kernel32.lib")
+//#pragma comment(linker,"\"//MANIFESTFILE:app.manifest\"")
+
+#include <WinBase.h>
+
+
+int main(int argc, char **argv)
+{
+	if (argc != 4) {
+		fprintf(stderr, "usage: %s hwnd msg wParam lParam");
+		return 1;
+	}
+
+	SendMessage(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+
+	return 0;
+}
