50d12ac105f2e901405f378ed549e02f1e08d602
ste <git@grundell.co.uk>
2019-07-11 22:27:43 +0100

    Compiling as a GUI application

--- SendMessage.c
+++ SendMessage.c
@@ -1,22 +1,25 @@
-﻿#include <stdio.h>
+﻿#define UNICODE 1
+#include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
 
 #pragma comment (lib, "User32.lib")
 #pragma comment (lib, "Kernel32.lib")
+#pragma comment (lib, "Shell32.lib")
 //#pragma comment(linker,"\"//MANIFESTFILE:app.manifest\"")
 
-#include <WinBase.h>
 
-
-int main(int argc, char **argv)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-	if (argc != 4) {
-		fprintf(stderr, "usage: %s hwnd msg wParam lParam", argv[0]);
+    int argc;
+    LPWSTR *argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+	if (argc != 5) {
+		fwprintf(stderr, L"usage: %s hwnd msg wParam lParam %u\n", argv[0], argc);
 		return 1;
 	}
 
-	SendMessage(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+	SendMessage((HWND)_wtoi(argv[1]), (UINT)_wtoi(argv[2]), (WPARAM)_wtoi(argv[3]), (LPARAM)_wtoi(argv[4]));
 
 	return 0;
 }
--- build.bat
+++ build.bat
@@ -3,8 +3,10 @@
 if "%WindowsSdkDir%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 ::$ENV:PATH += ";C:\Program Files (x86)\Windows Kits\8.1\bin\x86"
 
+del sendmessage.exe *.obj
+
 :: Compile/link
-cl SendMessage.c
+cl SendMessage.c /link /SUBSYSTEM:WINDOWS
 
 :: Add the manifest
 mt -manifest app.manifest "-outputresource:SendMessage.exe;#1"
